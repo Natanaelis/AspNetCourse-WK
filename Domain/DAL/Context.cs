2b5670eba3dcc12f997af07f2bea4ece5b8d21df
Wojciech Kubala <60062709+Natanaelis@users.noreply.github.com>
2021-03-27 12:52:45 +0100

    Create RESR API and Domain projects

---
+++ Domain/DAL/Context.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DAL
+{
+    public class Context : DbContext
+    {
+        DbSet<Price> Prices { get; set; }
+        public Context()
+        {
+            sqlServer = ConfigurationHelper.Configuration.GetConnetcionString("SQLServer");
+        }
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        private string sqlServer; // readonly ?
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (sqlServer != null)
+            {
+                optionsBuilder.UseSqlServer(sqlServer);
+            }
+        }
+    }
+}
