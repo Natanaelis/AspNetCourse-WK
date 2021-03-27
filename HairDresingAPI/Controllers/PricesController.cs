7b8aec5dcf0bf5eed61cc0391a4eea36dbdb7c30
Wojciech Kubala <60062709+Natanaelis@users.noreply.github.com>
2021-03-27 16:02:25 +0100

    Updates

---
+++ HairDresingAPI/Controllers/PricesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HairDresingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PricesController : ControllerBase
+    {
+        private readonly IPricesRepository repository;
+
+        public PricesController(IPricesRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(await repository.GetListAsync());
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
