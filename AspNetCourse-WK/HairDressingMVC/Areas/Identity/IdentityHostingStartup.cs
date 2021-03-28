df4ff2af8777bcc6137084f3718c2d9a69211414
Wojciech Kubala <60062709+Natanaelis@users.noreply.github.com>
2021-03-28 16:21:33 +0200

    Added Client MVC

---
+++ AspNetCourse-WK/HairDressingMVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HairDressingMVC.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HairDressingMVC.Areas.Identity.IdentityHostingStartup))]
+namespace HairDressingMVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
